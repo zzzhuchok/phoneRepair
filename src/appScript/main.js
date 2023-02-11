1ee8c64d0701715462110c74600facbadc75883d
Rostislav Zhuk <zzzhuchok@gmail.com>
2023-02-11 16:47:49 +0300

    feat: add showHistory

--- src/appScript/main.js
+++ src/appScript/main.js
@@ -1,9 +1,23 @@
+// function doGet() {
+//   const htmlService = HtmlService.createTemplateFromFile("index");
+//   const html = htmlService.evaluate();
+//   html.setTitle("Ремонт ТА");
+//   html.addMetaTag("viewport", "width=device-width, initial-scale=1");
+//   return html;
+// }
+
 function doGet() {
-  const htmlService = HtmlService.createTemplateFromFile("index");
-  const html = htmlService.evaluate();
-  html.setTitle("Ремонт ТА");
-  html.addMetaTag("viewport", "width=device-width, initial-scale=1");
-  return html;
+  const activeUser = Session.getActiveUser();
+  const activeUserEmail = activeUser.getEmail();
+
+  const data = {
+    firstName: "Ross",
+    email: activeUserEmail,
+  };
+
+  return ContentService.createTextOutput(JSON.stringify(data)).setMimeType(
+    ContentService.MimeType.JSON
+  );
 }
 
 function include(filename) {
